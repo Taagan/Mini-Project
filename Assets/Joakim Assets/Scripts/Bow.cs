a3aca5c9f76227f05aaedb077aa491e03d9ed328
JoakimTell <62691952+JoakimTell@users.noreply.github.com>
2021-05-03 14:48:58 +0200

    Lagt till animationer, framåt och bakåt. Lagt till så spelaren kan skjuta

--- Assets/Joakim Assets/Scripts/Bow.cs
+++ Assets/Joakim Assets/Scripts/Bow.cs
@@ -1,29 +1,54 @@
-
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
-    public float damage = 10f;
-    public float range = 100f;
+    [SerializeField] 
+    [Range(0.5f,1.5f)] 
+    private float fireRate = 1;
 
-    public Camera thirdPersonCam;
+    [SerializeField]
+    [Range(1, 10)]
+    private int damage = 1;
 
-    // Update is called once per frame
+    [SerializeField]
+    private AudioSource arrowShootingSource;
+
+    private float timer;
+
     void Update()
     {
-      if(Input.GetButtonDown("Fire1"))
+        timer += Time.deltaTime;
+        if(timer >= fireRate)
         {
-            Shoot();
-        }   
+            if (Input.GetButton("Fire1"))
+            {
+                timer = 0f;
+                FireBow();
+            }
+        }
     }
 
-    void Shoot()
+    private void FireBow()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(thirdPersonCam.transform.position, thirdPersonCam.transform.forward, out hit, range))
+
+        arrowShootingSource.Play();
+
+        Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
+        RaycastHit hitInfo;
+
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 2f);
+
+
+        if (Physics.Raycast(ray, out hitInfo, 100))
         {
-            Debug.Log(hit.transform.name);
+            var health = hitInfo.collider.GetComponent<Health>();
+
+            if(health != null)
+                health.TakeDamage(damage);
+          
         }
     }
 }
-
--- Assets/Joakim Assets/Scripts/PlayerMovement_Archer.cs
+++ Assets/Joakim Assets/Scripts/PlayerMovement_Archer.cs
@@ -22,15 +22,17 @@ public class PlayerMovement_Archer : MonoBehaviour
 
         var movement = new Vector3(horizontal, 0, vertical);
 
-        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+        animator.SetFloat("Speed", vertical);
 
-        animator.SetFloat("Speed", movement.magnitude);
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
 
-        if(movement.magnitude > 0)
+        if(vertical != 0)
         {
-            Quaternion newDirection = Quaternion.LookRotation(movement);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+            characterController.SimpleMove(transform.forward * moveSpeed * vertical);
         }
+
+   
+
+       
     }
 }
