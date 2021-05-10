b2ba198c4b9cdea332c450ca4d4609e1eb55daf7
JoakimTell <62691952+JoakimTell@users.noreply.github.com>
2021-05-10 15:53:18 +0200

    Lite fix och trix med animationer. Strul för tillfället med walkanimationen

--- Assets/Joakim Assets/Scripts/AnimationMovementController.cs
+++ Assets/Joakim Assets/Scripts/AnimationMovementController.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class AnimationMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private MovementInputController movement;
+
+    private Vector2 smoothDeltaPos = Vector2.zero;
+    public Vector2 velocity = Vector2.zero;
+    public float magnitude = 0.25f;
+    public bool moving;
+
+    private void OnEnable()
     {
-        
+        movement = GetComponent<MovementInputController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 worldPosition = movement.nextPosition - transform.position;
+
+        float x = Vector3.Dot(transform.right, worldPosition);
+        float y = Vector3.Dot(transform.forward, worldPosition);
+        Vector2 deltaPosition = new Vector2(x, y);
+
+        float smoothing = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothDeltaPos = Vector2.Lerp(smoothDeltaPos, deltaPosition, smoothing);
+
+        if (Time.deltaTime > 1e-5f)
+        {
+            velocity = smoothDeltaPos / Time.deltaTime;
+        }
+
+        moving = velocity.magnitude > magnitude;
+
+        bool isAiming = (movement.aimInput == 1f);
+        bool fire = (movement.fireInput == 1f);
+
+        if (isAiming)
+        {
+            if (fire)
+            {
+                animator.SetTrigger("Fire");
+                //Skjut pil-metod
+            }
+            if (animator.GetCurrentAnimatorStateInfo(2).IsName("Fire"))
+            {
+                //Avaktivera någon form av projektil
+            }
+            else
+            {
+                //aktivera pil
+            }
+        }
+
+        animator.SetBool("Aiming", isAiming);
+        animator.SetBool("Moving", moving);
+        animator.SetFloat("VelocityX", velocity.x);
+        animator.SetFloat("VelocityY", Mathf.Abs(velocity.y));
     }
+
+    private void OnAnimatorMove()
+    {
+        transform.position = movement.nextPosition;
+    }
+
+
 }
--- Assets/Joakim Assets/Scripts/MovementInputController.cs
+++ Assets/Joakim Assets/Scripts/MovementInputController.cs
@@ -1,18 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(PlayerInput))]
 public class MovementInputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 move;
+    public Vector2 look;
+    public float aimInput;
+    public float fireInput;
+
+    public Vector3 nextPosition;
+    public Quaternion nextRotation;
+
+    public float rotationPower = 3f;
+    public float rotationLerp = 0.5f;
+
+    public float speed = 1f;
+    public Camera mainCamera;
+    public GameObject followTransform;
+ 
+    public void OnMove(InputValue value)
     {
-        
+        move = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnLook(InputValue value)
     {
-        
+        look = value.Get<Vector2>();
     }
+
+    public void OnAim(InputValue value)
+    {
+        aimInput = value.Get<float>();
+    }
+
+    public void OnFire(InputValue value)
+    {
+        fireInput = value.Get<float>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+    private void Update()
+    {
+        followTransform.transform.rotation *= Quaternion.AngleAxis(look.x * rotationPower, Vector3.up);
+        followTransform.transform.rotation *= Quaternion.AngleAxis(look.y * rotationPower, Vector3.right);
+
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 300)
+        {
+            angles.x = 300;
+        }
+        else if (angle < 180 && angle > 40)
+        {
+            angles.x = 40;
+        }
+
+
+        followTransform.transform.localEulerAngles = angles;
+        nextRotation = Quaternion.Lerp(followTransform.transform.rotation, nextRotation, Time.deltaTime * rotationLerp);
+
+        if (move.x == 0 && move.y == 0)
+        {
+            nextPosition = transform.position;
+
+            if (aimInput == 1)
+            {
+                transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+                followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+            }
+
+            return;
+        }
+        float moveSpeed = speed / 100f;
+        Vector3 position = (transform.forward * move.y * moveSpeed) + (transform.right * move.x * moveSpeed);
+        nextPosition = transform.position + position;
+
+        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+    }
+
+
 }
